1d383435f0e1b10da6a87041322d9424610ec814
saarangt <saarang.tiwari@hotmail.com>
2018-07-23 20:58:47 +0530

    Added three basic ways to implement the canvas

---
+++ app/src/main/java/com/developer/saarang/coregraphics/MyCustomView.java
@@ -0,0 +1,16 @@
+package com.developer.saarang.coregraphics;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class MyCustomView extends View {
+    public MyCustomView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
